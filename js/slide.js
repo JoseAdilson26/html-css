e6c46e88d7ef2393084c6e7f1ba52697077a730b
José Batalha <zenildo93jos@gmail.com>
2023-08-09 16:09:32 +0100

    atualização do site

---
+++ js/slide.js
@@ -0,0 +1,14 @@
+'use strict' 
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+const slideList = document.querySelector('[data-slide="list"]')
+const navPreviousButton = document.querySelector('[data-slide="nav-previous-button"]')
+const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
+const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
+const slideItem = document.querySelectorAll('[data-slide="item"]')
+const controlsButton = document.querySelectorAll('[data-slide="control-button"]')
+
+slideItem.forEach (function(slideItem, index){
+    slideItem.addEventListner ()
+   
+}) 
